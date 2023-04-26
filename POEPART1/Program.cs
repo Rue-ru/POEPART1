82461903c09553e46194c8811c3d72353aa4a4c2
rue_m <rue_m@LAPTOP>
2023-04-26 13:17:53 +0200

    Add project files.

---
+++ POEPART1/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+namespace POE_part1
+{
+    public class Program
+    {
+
+        static void Main(string[] args)
+        {
+            int noOfIngredients;
+            String ingredientName;
+            int quantity;
+            string recipeUnitsofMeasure;
+            int recipeSteps;
+            string description;
+
+            Console.WriteLine("Enter the number of ingredients you would like to enter");
+            noOfIngredients = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 0; i < noOfIngredients; i++)
+            {
+
+                Console.WriteLine("Enter the ingredients name with capital letters");
+                //enter the ingredients you want 
+                ingredientName = Console.ReadLine();
+
+                Console.WriteLine("Enter the ingredients quantity");
+                quantity = Convert.ToInt32(Console.ReadLine());
+
+                Console.WriteLine("Enter the unit of measurement");
+                recipeUnitsofMeasure = Console.ReadLine();
+
+            }
+
+            Console.WriteLine("Enter the number of steps you would like to add");
+            recipeSteps = Convert.ToInt32(Console.ReadLine());
+
+            for (int b = 0; b < recipeSteps; b++)
+            {
+                Console.WriteLine("Please enter your steps");
+                string steps = Console.ReadLine();
+            }
+        }
+
+    }
+}
