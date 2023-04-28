06d59f2e167442c8385b2f4583b1843680ecb3dd
Rue-ru <st10186913@imconnect.edu.za>
2023-04-28 19:05:37 +0200

    Adding second class

---
+++ POEPART1/Recipe.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace POEPART1
+{
+    public class Recipe
+    {
+
+        public string IngredientName { get; set; }
+        public int Quantity { get; set; }
+        public int RecipeUnitsofMeasure { get; set; }
+        public int RecipeSteps { get; set; }
+        public string Description { get; set; }
+
+        public Recipe(string ingredientName, int quantity, int recipeUnitsofMeasure, string description)
+        {
+            IngredientName = ingredientName;
+            Quantity = quantity;    
+            RecipeUnitsofMeasure = recipeUnitsofMeasure;
+            Description = description;
+
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine();
+
+
+        }
+    }
+}
